f8c7d39da553eeb154fa27c4e0b9f83a6286e0e4
Nam <91453554+NamDT69@users.noreply.github.com>
2021-12-16 11:58:15 +0700

    thay doi ty

--- t/test.cpp
+++ t/test.cpp
@@ -19,3 +19,15 @@ int main(){
 	std::cout << a.at(0) << " "<< a[0];
 	std::cout << " " << *(a.data() + 1);
 }
+
+
+
+127.0.0.1	localhost
+127.0.1.1	Magician
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
